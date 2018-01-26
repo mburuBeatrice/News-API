55ab5c8de807eb51190c3ab651107225b636a6ca
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-01-26 15:48:01 +0300

    add a new route that has a news view function

---
+++ app/views.py
@@ -0,0 +1,20 @@
+from flask import render_template
+from app import app
+
+#views
+@app.route('/')
+def index():
+
+    """
+    View root page function that returns the index page and its data
+    """
+    message = "News article"
+    return render_template('index.html')
+@app.route('/news/<news_id>')
+def news(news_id):
+    """
+    View news page function that returns news details page
+    page and its data
+    """
+    return render_template('news.html',id = news_id)
+     
