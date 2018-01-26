0107cf38d23969137f1fe70f2ffd1e2696db4aea
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-01-26 16:12:15 +0300

    add configuration classes

--- app/views.py
+++ app/views.py
@@ -10,11 +10,10 @@ def index():
     """
     message = "News article"
     return render_template('index.html')
-@app.route('/news/<news_id>')
+@app.route('/news/<int:news_id>')
 def news(news_id):
     """
     View news page function that returns news details page
     page and its data
     """
     return render_template('news.html',id = news_id)
-     
