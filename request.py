60f83ff640710d22ac3a8757163245bbf71b50f3
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-01-26 17:33:08 +0300

    add first api call

--- request.py
+++ request.py
@@ -2,3 +2,24 @@ from app import app
 
 # Getting api key
 api_key = app.config['NEWS_API_KEy']
+
+# Getting the news base url
+base_url = app.config["NEWS_API_BASE_URL"]
+
+def get_news(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_news_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['articles']:
+            news_articles_list = get_news_response['articles']
+            news_articles = process_articles(news_articles_list)
+
+    return news_articles        
