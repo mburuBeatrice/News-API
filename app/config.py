937d2a1c44bb45fb4a9875f882e70a6574cc4d3e
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-01-29 02:07:50 +0300

    add changes to news api base url

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey=4cfe0bc8309c4a0db2f6ecaa268fcee9'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=&category={}&apiKey=4cfe0bc8309c4a0db2f6ecaa268fcee9'
 
 
 
